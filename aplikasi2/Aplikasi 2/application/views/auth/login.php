42b4768fbda466035e300f4909e25fd5b4c38193
Dian Asmara Dahana <dahonobambanghandoko@gmail.com>
2020-08-11 11:24:21 -1200

    update view

--- aplikasi2/Aplikasi 2/application/views/auth/login.php
+++ aplikasi2/Aplikasi 2/application/views/auth/login.php
@@ -21,16 +21,16 @@
 
 </head>
 
-<body style="background-image: url(<?php echo base_url('assets/image/bg.jpg')?>); background-size: 100% 100%">
+<body style="background-image: url(<?php echo base_url('assets/image/gambarbps.jpeg')?>); background-size: 100% 100%">
 
-<div class="container" style=" margin-top: 73px; ">
+<div class="container" style=" margin-top: 73px;">
 
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-      <div class="col-lg-7 ">
+    <div class="col-xl-5 col-lg-12 col-md-9">
 
-        <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card o-hidden border-0 shadow-lg my-5" style = "background-color : grey">
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
@@ -38,11 +38,11 @@
                 <div class="p-5">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Login</h1>
-                    <p style="color: white; margin-bottom: 20px; font-family: Viga;"><em>"Masukan username dan password anda!"</em></p>
+                    <p style="color: black; margin-bottom: 20px; font-family: Viga;"><em>"Masukan username dan password anda!"</em></p>
                   </div>
                   <form class="user">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input type="text" class="form-control form-control-user" id="exampleInputUsername" aria-describedby="emailHelp" placeholder="Username">
                     </div>
                     <div class="form-group">
                       <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
@@ -57,10 +57,8 @@
                     </a>
                   </form>
                   <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                    <a class="small" href="<?=base_url('c_lupapassword'); ?>">Forgot Password?</a>
                   </div>
-                  <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
                   </div>
                 </div>
               </div>
